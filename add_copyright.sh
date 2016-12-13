db7fa606cc972bdfa7dc599393d60da72256eb51
Younes Nejahi <younes@wayne.edu>
2016-12-12 21:02:05 -0500

    add copyright to files with *.cc *.cpp and *.h extensions.

---
+++ add_copyright.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for i in *.cc
+do
+    if ! grep -q COPYRIGHT.txt $i
+	then
+	cat cp.txt $i >$i.new && mv $i.new $i
+	fi
+done
+
+for i in *.cpp
+do
+    if ! grep -q COPYRIGHT.txt $i
+	then
+	cat cp.txt $i >$i.new && mv $i.new $i
+	fi
+done
+
+for i in *.h
+do
+    if ! grep -q COPYRIGHT.txt $i
+	then
+	cat cp.txt $i >$i.new && mv $i.new $i
+	fi
+done
